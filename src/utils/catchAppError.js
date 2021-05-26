fb849b5a50cd8eb79e1097223c7f47d31686e5a6
Ivan <raggamix@mail.ru>
2021-05-26 18:38:05 +0300

    docs: add jsdoc in catch error files

--- src/utils/catchAppError.js
+++ src/utils/catchAppError.js
@@ -1,6 +1,15 @@
+const { NOT_FOUND } = require('http-status-codes');
+/**
+ * Wrapper for catch app.js errors
+ * @param {Error} err - Express catch error
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express next middleware function
+ * @returns {Function} return catch status code
+ */
 const catchAppError = (err, req, res, next) => {
     if (err.code === 'ERR_ENTITY_NOT_FOUND') {
-      res.status(404).send('Something failed')
+      res.status(NOT_FOUND).send('Something failed')
     } else {
       next(err);
     }
--- src/utils/catchError.js
+++ src/utils/catchError.js
@@ -1,5 +1,9 @@
 const { NOT_FOUND } = require('http-status-codes');
-
+/**
+ * Wrapper for catch router errors
+ * @param {Function} fn for Express middleware function
+ * @returns {Function} return catch status code
+ */
 const catchError = (fn) => (req, res, next) =>
   fn(req, res, next).catch((err) => {
     res.status(NOT_FOUND).send(err.message);
