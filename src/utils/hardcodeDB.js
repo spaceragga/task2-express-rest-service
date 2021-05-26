b4ac8f52bb8e6ff6988c7149e53dd7c00a33c768
Ivan <raggamix@mail.ru>
2021-05-26 19:05:10 +0300

    docs: add jsdoc in db file

--- src/utils/hardcodeDB.js
+++ src/utils/hardcodeDB.js
@@ -3,24 +3,50 @@ const db = {
   Users: [],
   Boards: [],
 };
-
+/**
+ * Function for getting all objects in db by parameter
+ * @param {String} tableName - name property in db
+ * @returns {Object[]} - return array of objects
+ */
 const getAllEntities = async (tableName) =>
   JSON.parse(JSON.stringify(db[tableName]));
-
+/**
+ * Function for getting objects in db by id
+ * @param {String} tableName - name property
+ * @param {String} id - id parameter
+ * @returns {Object} - return object
+ */
 const getEntity = async (tableName, id) =>
   db[tableName].find((entity) => entity.id === id);
-
+/**
+ * Function for create objects in db by parameters
+ * @param {String} tableName - name property in db
+ * @param {Object} entity - object for push in array
+ * @returns {Object} - return object
+ */
 const createEntity = async (tableName, entity) => {
   db[tableName].push(entity);
   return entity;
 };
-
+/**
+ * Function for update objects in db by parameters
+ * @param {String} tableName - name property in db
+ * @param {String} id - id parameter
+ * @param {Object} props - data for update
+ * @returns {Object} - return object
+ */
 const updateEntity = async (tableName, id, props) => {
   const entity = await getEntity(tableName, id);
   Object.assign(entity, { ...props });
   return entity;
 };
+/**
+ * Function for delete objects in db by parameters
+ * @param {String} tableName - name property in db
+ * @param {String} id - id parameter for delete
+ * @returns {void} return undefined
 
+ */
 const deleteEntity = async (tableName, id) => {
   switch (tableName) {
     case 'Users':
